c162f17b15b17e79d1daa2e07b107a734f00dbdb
seashelly <leesohyun@snu.ac.kr>
2025-11-11 14:15:23 +0900

    refactor: Do fizzbuzz in 1 if statement

--- fb.py
+++ fb.py
@@ -1,9 +1,5 @@
 for j in range(4, 15+1):
-    if j % 15  == 0:
-        print('fizzbuzz')
-    if j % 5  == 0:
-        print('buzz')
-    elif j % 3 == 0:
-        print('fizz')
+    if j % 3  == 0 or j % 5 == 0:
+        print('fizz' * (j%3==0) + 'buzz' * (j%5==0))
     else:
         print(j)
